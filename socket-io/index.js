99d009676560f594b04e9706f15c95acce9fca59
hafizmuhammadshoaib <shoaibsilat9@gmail.com>
2018-11-17 01:45:06 +0500

    basic of message receiving completed

--- socket-io/index.js
+++ socket-io/index.js
@@ -1,9 +1,18 @@
 module.exports = function (io) {
     io.on('connection', (socket) => {
         console.log('A client just joined on', socket.id);
+        // socket.on("message", (message) => {
+        //     console.log(message);
+        //     socket.emit("messages", message)
+        // })
         socket.on("message", (message) => {
-            console.log(message);
-            socket.emit("messages", message)
-        })
+            let json = JSON.parse(`{${message}}`);
+            console.log("json", json);
+            io.emit("HU 02", json);
+        });
+        
+        socket.on('disconnect', function () {
+            console.log("disconnected", socket.id)
+        });
     });
 }
