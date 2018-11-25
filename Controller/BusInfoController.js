d18681146f7a7113c837b6f3ed7b54f66b535861
hafizmuhammadshoaib <shoaibsilat9@gmail.com>
2018-11-26 00:19:04 +0500

    get all point info route updated and HU-38 route implemented on database

--- Controller/BusInfoController.js
+++ Controller/BusInfoController.js
@@ -1,7 +1,7 @@
 var BusInfo = require('../Model/BusInfoModel');
 var throwError = require("../ErrorHandler/Error");
 class BusInfoClass {
-    static getInfo(page, res, next) {
+    static getInfoByPage(page, res, next) {
         var perPage = 10
         var page = page || 1;
         BusInfo.find({})
@@ -32,6 +32,15 @@ class BusInfoClass {
                     }
                 })
             })
+    }
+    static getAllInfo(res, next) {
+        BusInfo.find({}, (err, data) => {
+            if (!err) {
+                res.json({ status: "success", info: [...data] });
+            } else {
+                throwError(err.message, 500, next)
+            }
+        })
     }
 }
 
--- routes/BusInfo.js
+++ routes/BusInfo.js
@@ -3,7 +3,10 @@ var router = express.Router();
 var InfoController = require("../Controller/BusInfoController");
 var verifyToken = require("../TokenVerifier/TokenVerifier")
 
+router.get("/all", verifyToken, (req, res, next) => {
+    InfoController.getAllInfo(res, next);
+})
 router.get("/:page?", verifyToken, (req, res, next) => {
-    InfoController.getInfo(req.query.page, res, next)
+    InfoController.getInfoByPage(req.query.page, res, next);
 })
 module.exports = router;
