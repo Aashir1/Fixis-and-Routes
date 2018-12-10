991a11b96e683602d2e9ccbcd5e19ef0ce817e12
aashir1 <kaashir9@gmail.com>
2018-12-10 15:07:22 +0500

    crud operation

--- routes/BusInfo.js
+++ routes/BusInfo.js
@@ -5,5 +5,18 @@ var verifyToken = require("../TokenVerifier/TokenVerifier")
 
 router.get("/:page?", verifyToken, (req, res, next) => {
     InfoController.getInfo(req.query.page, res, next)
+});
+
+
+router.post('/add', verifyToken, (req, res, next) => {
+    InfoController.addInfo(req, res, next);
+});
+
+router.post('/delete/:id', verifyToken, (req, res, next) => {
+    InfoController.deleteInfo(req, res, next);
+})
+router.post('/update', verifyToken, (req, res, next) => {
+    console.log("update bus info: ", req.body);
+    InfoController.updateInfo(req, res, next);
 })
 module.exports = router;
